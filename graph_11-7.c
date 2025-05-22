c3b521e4f6aad6ca5c0af95f46de69a7556af35a
kyumin <kyumin1429@naver.com>
2025-05-22 11:06:10 +0900

    05/22

--- graph_11-7.c
+++ graph_11-7.c
@@ -2,33 +2,44 @@
 #include <stdlib.h>
 #include <time.h>
 
-// 완전 가중치 그래프를 무작위로 생성하여 파일로 저장하는 함수
-void generate_weighted_complete_graph(const char* filename, int nVtx, int minW, int maxW) {
-    FILE* fp = fopen(filename, "w");
+// 무작위 가중치 그래프를 파일로 저장하는 함수 (매개변수: nVtx, nEdge)
+void random_graph(int nVtx, int nEdge) {
+    int minW = 1, maxW = 20;
+    FILE* fp = fopen("random_graph.txt", "w");
     if (!fp) {
         printf("파일을 열 수 없습니다.\n");
         return;
     }
     srand((unsigned int)time(NULL));
-    fprintf(fp, "%d\n", nVtx);
-    for (int i = 0; i < nVtx; i++) {
-        fprintf(fp, "%c ", 'A' + i); // 정점 이름
-        for (int j = 0; j < nVtx; j++) {
-            if (i == j)
-                fprintf(fp, "0 ");
-            else
-                fprintf(fp, "%d ", minW + rand() % (maxW - minW + 1));
-        }
-        fprintf(fp, "\n");
+    fprintf(fp, "%d %d\n", nVtx, nEdge);
+
+    // 간선 중복 방지용 배열
+    int** edge = (int**)calloc(nVtx, sizeof(int*));
+    for (int i = 0; i < nVtx; i++)
+        edge[i] = (int*)calloc(nVtx, sizeof(int));
+
+    int cnt = 0;
+    while (cnt < nEdge) {
+        int u = rand() % nVtx;
+        int v = rand() % nVtx;
+        if (u == v || edge[u][v]) continue;
+        int w = minW + rand() % (maxW - minW + 1);
+        fprintf(fp, "%c %c %d\n", 'A' + u, 'A' + v, w);
+        edge[u][v] = edge[v][u] = 1;
+        cnt++;
     }
+
+    for (int i = 0; i < nVtx; i++)
+        free(edge[i]);
+    free(edge);
+
     fclose(fp);
 }
 
 int main() {
-    int nVtx = 6; // 정점 수
-    int minW = 1; // 최소 가중치
-    int maxW = 20; // 최대 가중치
-    generate_weighted_complete_graph("random_graph.txt", nVtx, minW, maxW);
+    int nVtx = 6;   // 정점 수
+    int nEdge = 10; // 간선 수
+    random_graph(nVtx, nEdge);
     printf("random_graph.txt 파일이 생성되었습니다.\n");
     return 0;
 }
