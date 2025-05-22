dd02a57cf5f98142963493f4f8cb44616a674946
kyumin <kyumin1429@naver.com>
2025-05-22 11:12:44 +0900

    05/22

--- graph_11-9.c
+++ graph_11-9.c
@@ -4,57 +4,44 @@
 
 #define MAX_VTXS 100
 
-// 연결된 완전 가중치 그래프 생성 함수
-void generate_connected_weighted_complete_graph(const char* filename, int nVtx, int minW, int maxW) {
-    int adj[MAX_VTXS][MAX_VTXS] = {0};
-    int used[MAX_VTXS] = {0};
-    int i, j;
-
-    srand((unsigned int)time(NULL));
-
-    // 1. MST(임의의 트리) 간선 추가 (연결 보장)
-    used[0] = 1;
-    for (i = 1; i < nVtx; i++) {
-        int u = rand() % i; // 이미 트리에 포함된 정점 중 하나
-        int w = minW + rand() % (maxW - minW + 1);
-        adj[i][u] = adj[u][i] = w;
-        used[i] = 1;
-    }
-
-    // 2. 나머지 간선도 무작위로 추가 (완전 그래프)
-    for (i = 0; i < nVtx; i++) {
-        for (j = 0; j < nVtx; j++) {
-            if (i != j && adj[i][j] == 0) {
-                int w = minW + rand() % (maxW - minW + 1);
-                adj[i][j] = w;
-            }
-        }
-    }
-
-    // 3. 파일로 저장
-    FILE* fp = fopen(filename, "w");
+// 무작위 가중치 그래프를 파일로 저장하는 함수 (매개변수: nVtx, nEdge)
+void random_graph(int nVtx, int nEdge) {
+    int minW = 1, maxW = 20;
+    FILE* fp = fopen("random_graph.txt", "w");
     if (!fp) {
         printf("파일을 열 수 없습니다.\n");
         return;
     }
-    fprintf(fp, "%d\n", nVtx);
-    for (i = 0; i < nVtx; i++) {
-        fprintf(fp, "%c ", 'A' + i);
-        for (j = 0; j < nVtx; j++) {
-            if (i == j)
-                fprintf(fp, "0 ");
-            else
-                fprintf(fp, "%d ", adj[i][j]);
-        }
-        fprintf(fp, "\n");
+    srand((unsigned int)time(NULL));
+    fprintf(fp, "%d %d\n", nVtx, nEdge);
+
+    // 간선 중복 방지용 배열
+    int** edge = (int**)calloc(nVtx, sizeof(int*));
+    for (int i = 0; i < nVtx; i++)
+        edge[i] = (int*)calloc(nVtx, sizeof(int));
+
+    int cnt = 0;
+    while (cnt < nEdge) {
+        int u = rand() % nVtx;
+        int v = rand() % nVtx;
+        if (u == v || edge[u][v]) continue;
+        int w = minW + rand() % (maxW - minW + 1);
+        fprintf(fp, "%c %c %d\n", 'A' + u, 'A' + v, w);
+        edge[u][v] = edge[v][u] = 1;
+        cnt++;
     }
+
+    for (int i = 0; i < nVtx; i++)
+        free(edge[i]);
+    free(edge);
+
     fclose(fp);
 }
 
 int main() {
-    int nVtx = 8; // 정점 수
-    int minW = 1, maxW = 20; // 가중치 범위
-    generate_connected_weighted_complete_graph("random_connected_graph.txt", nVtx, minW, maxW);
-    printf("random_connected_graph.txt 파일이 생성되었습니다.\n");
+    int nVtx = 8;   // 정점 수
+    int nEdge = 12; // 간선 수
+    random_graph(nVtx, nEdge);
+    printf("random_graph.txt 파일이 생성되었습니다.\n");
     return 0;
 }
