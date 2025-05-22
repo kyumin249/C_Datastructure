5632dc2f3736406a0a80d64a12990d0f636cfe0b
kyumin <kyumin1429@naver.com>
2025-05-22 12:04:15 +0900

    05/22

--- graph_11-9.c
+++ graph_11-9.c
@@ -19,6 +19,7 @@ void save_adj_matrix_to_file(int nVtx) {
         fprintf(fp, "\n");
     }
     fclose(fp);
+    printf("adj_matrix.txt 파일이 생성되었습니다.\n");
 }
 
 void count_connected_components(int nVtx) {
@@ -52,86 +53,30 @@ void count_connected_components(int nVtx) {
     printf("\n");
 }
 
-void random_graph(int nVtx, int nEdge) {
-    int minW = 1, maxW = 20;
-    FILE* fp = fopen("random_graph.txt", "w");
-    if (!fp) {
-        printf("파일을 열 수 없습니다.\n");
-        return;
-    }
+void generate_random_adj_matrix(int nVtx, int nEdge) {
     srand((unsigned int)time(NULL));
-    fprintf(fp, "%d %d\n", nVtx, nEdge);
-
-    // 간선 중복 방지용 배열
-    int** edge = (int**)calloc(nVtx, sizeof(int*));
-    for (int i = 0; i < nVtx; i++) {
-        edge[i] = (int*)calloc(nVtx, sizeof(int));
+    for (int i = 0; i < nVtx; i++)
         for (int j = 0; j < nVtx; j++)
-            adj[i][j] = 0;  // 인접행렬 초기화
-    }
+            adj[i][j] = 0;
 
-    int cnt = 0;
-    while (cnt < nEdge) {
+    int count = 0;
+    while (count < nEdge) {
         int u = rand() % nVtx;
         int v = rand() % nVtx;
-        if (u == v || edge[u][v]) continue;
-        int w = minW + rand() % (maxW - minW + 1);
-        fprintf(fp, "%c %c %d\n", 'A' + u, 'A' + v, w);
-        edge[u][v] = edge[v][u] = 1;
-        adj[u][v] = adj[v][u] = 1;
-        cnt++;
+        if (u == v || adj[u][v]) continue;
+        adj[u][v] = adj[v][u] = 1; // 무방향 그래프
+        count++;
     }
-
-    for (int i = 0; i < nVtx; i++)
-        free(edge[i]);
-    free(edge);
-    fclose(fp);
-}
-
-void random_connected_graph(int nVtx) {
-    int minW = 1, maxW = 20;
-    int nEdge = nVtx - 1;
-    FILE* fp = fopen("random_connected_graph.txt", "w");
-    if (!fp) {
-        printf("파일을 열 수 없습니다.\n");
-        return;
-    }
-    srand((unsigned int)time(NULL));
-    fprintf(fp, "%d %d\n", nVtx, nEdge);
-
-    int** edge = (int**)calloc(nVtx, sizeof(int*));
-    for (int i = 0; i < nVtx; i++) {
-        edge[i] = (int*)calloc(nVtx, sizeof(int));
-    }
-
-    for (int i = 1; i < nVtx; i++) {
-        int u = i;
-        int v = rand() % i;
-        int w = minW + rand() % (maxW - minW + 1);
-        fprintf(fp, "%c %c %d\n", 'A' + u, 'A' + v, w);
-        edge[u][v] = edge[v][u] = 1;
-    }
-
-    for (int i = 0; i < nVtx; i++)
-        free(edge[i]);
-    free(edge);
-    fclose(fp);
 }
 
 int main() {
     int nVtx = 8;
     int nEdge = 12;
 
-    random_graph(nVtx, nEdge);
-    printf("random_graph.txt 파일이 생성되었습니다.\n");
-
+    generate_random_adj_matrix(nVtx, nEdge);
     save_adj_matrix_to_file(nVtx);
-    printf("adj_matrix.txt 파일이 생성되었습니다.\n");
-
     count_connected_components(nVtx);
 
-    random_connected_graph(nVtx);
-    printf("random_connected_graph.txt 파일이 생성되었습니다.\n");
-
     return 0;
-}//(11.1-3)
+}
+//(11.1-3)
