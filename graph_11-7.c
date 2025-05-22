6fa991d13daf0b44d71c7697054d4507306116e6
kyumin <kyumin1429@naver.com>
2025-05-22 11:23:05 +0900

    05/22

--- graph_11-7.c
+++ graph_11-7.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-// 무작위 가중치 그래프를 파일로 저장하는 함수 (매개변수: nVtx, nEdge)
+// 무작위 0, 1 그래프를 파일로 저장하는 함수 (매개변수: nVtx, nEdge)
 void random_graph(int nVtx, int nEdge) {
-    int minW = 1, maxW = 20;
     FILE* fp = fopen("random_graph.txt", "w");
     if (!fp) {
         printf("파일을 열 수 없습니다.\n");
@@ -13,30 +12,36 @@ void random_graph(int nVtx, int nEdge) {
     srand((unsigned int)time(NULL));
     fprintf(fp, "%d %d\n", nVtx, nEdge);
 
-    // 간선 중복 방지용 배열
-    int** edge = (int**)calloc(nVtx, sizeof(int*));
+    // 인접 행렬 생성 및 0으로 초기화
+    int** adj = (int**)calloc(nVtx, sizeof(int*));
     for (int i = 0; i < nVtx; i++)
-        edge[i] = (int*)calloc(nVtx, sizeof(int));
+        adj[i] = (int*)calloc(nVtx, sizeof(int));
 
     int cnt = 0;
     while (cnt < nEdge) {
         int u = rand() % nVtx;
         int v = rand() % nVtx;
-        if (u == v || edge[u][v]) continue;
-        int w = minW + rand() % (maxW - minW + 1);
-        fprintf(fp, "%c %c %d\n", 'A' + u, 'A' + v, w);
-        edge[u][v] = edge[v][u] = 1;
+        if (u == v || adj[u][v]) continue;
+        adj[u][v] = adj[v][u] = 1;
         cnt++;
     }
 
+    // 파일에 인접행렬 저장
+    for (int i = 0; i < nVtx; i++) {
+        for (int j = 0; j < nVtx; j++) {
+            fprintf(fp, "%d ", adj[i][j]);
+        }
+        fprintf(fp, "\n");
+    }
+
     for (int i = 0; i < nVtx; i++)
-        free(edge[i]);
-    free(edge);
+        free(adj[i]);
+    free(adj);
 
     fclose(fp);
 }
 
-// 생성된 그래프 파일을 읽어와 출력하는 함수
+// 생성된 그래프 파일을 읽어와 직사각형(인접행렬)으로 출력하는 함수
 void print_graph(const char* filename) {
     FILE* fp = fopen(filename, "r");
     if (!fp) {
@@ -44,14 +49,19 @@ void print_graph(const char* filename) {
         return;
     }
     int nVtx, nEdge;
-    printf("생성된 그래프:\n");
-    if (fscanf(fp, "%d %d", &nVtx, &nEdge) == 2) {
-        printf("정점 수: %d, 간선 수: %d\n", nVtx, nEdge);
-        char u, v;
-        int w;
-        while (fscanf(fp, " %c %c %d", &u, &v, &w) == 3) {
-            printf("%c - %c : %d\n", u, v, w);
+    if (fscanf(fp, "%d %d", &nVtx, &nEdge) != 2) {
+        fclose(fp);
+        return;
+    }
+    printf("정점 수: %d, 간선 수: %d\n", nVtx, nEdge);
+    printf("(11.1-1)인접행렬(0, 1 그래프):\n");
+    int val;
+    for (int i = 0; i < nVtx; i++) {
+        for (int j = 0; j < nVtx; j++) {
+            fscanf(fp, "%d", &val);
+            printf("%d ", val);
         }
+        printf("\n");
     }
     fclose(fp);
 }
