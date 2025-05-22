91349ae358bab704db6d6ec2daa8e9a6130fe540
kyumin <kyumin1429@naver.com>
2025-05-22 11:27:38 +0900

    05/22

--- graph_11-8.c
+++ graph_11-8.c
@@ -4,9 +4,8 @@
 
 #define MAX_VTXS 100
 
-// 무작위 가중치 그래프를 파일로 저장하는 함수 (매개변수: nVtx, nEdge)
+// 무작위 0, 1 그래프를 파일로 저장하는 함수 (매개변수: nVtx, nEdge)
 void random_graph(int nVtx, int nEdge) {
-    int minW = 1, maxW = 20;
     FILE* fp = fopen("random_graph.txt", "w");
     if (!fp) {
         printf("파일을 열 수 없습니다.\n");
@@ -15,25 +14,31 @@ void random_graph(int nVtx, int nEdge) {
     srand((unsigned int)time(NULL));
     fprintf(fp, "%d %d\n", nVtx, nEdge);
 
-    // 간선 중복 방지용 배열
-    int** edge = (int**)calloc(nVtx, sizeof(int*));
+    // 인접 행렬 생성 및 0으로 초기화
+    int** adj = (int**)calloc(nVtx, sizeof(int*));
     for (int i = 0; i < nVtx; i++)
-        edge[i] = (int*)calloc(nVtx, sizeof(int));
+        adj[i] = (int*)calloc(nVtx, sizeof(int));
 
     int cnt = 0;
     while (cnt < nEdge) {
         int u = rand() % nVtx;
         int v = rand() % nVtx;
-        if (u == v || edge[u][v]) continue;
-        int w = minW + rand() % (maxW - minW + 1);
-        fprintf(fp, "%c %c %d\n", 'A' + u, 'A' + v, w);
-        edge[u][v] = edge[v][u] = 1;
+        if (u == v || adj[u][v]) continue;
+        adj[u][v] = adj[v][u] = 1;
         cnt++;
     }
 
+    // 파일에 인접행렬 저장
+    for (int i = 0; i < nVtx; i++) {
+        for (int j = 0; j < nVtx; j++) {
+            fprintf(fp, "%d ", adj[i][j]);
+        }
+        fprintf(fp, "\n");
+    }
+
     for (int i = 0; i < nVtx; i++)
-        free(edge[i]);
-    free(edge);
+        free(adj[i]);
+    free(adj);
 
     fclose(fp);
 }
@@ -49,19 +54,12 @@ void load_graph(const char* filename) {
         printf("그래프 파일을 열 수 없습니다.\n");
         exit(1);
     }
-    fscanf(fp, "%d %*d", &vsize); // 정점 수만 읽고, 간선 수는 무시
-    char u, v;
-    int w;
-    // 인접 행렬 초기화
+    int nEdge;
+    fscanf(fp, "%d %d", &vsize, &nEdge);
+    // 인접 행렬 읽기
     for (int i = 0; i < vsize; i++)
         for (int j = 0; j < vsize; j++)
-            adj[i][j] = 0;
-    while (fscanf(fp, " %c %c %d", &u, &v, &w) == 3) {
-        int i = u - 'A';
-        int j = v - 'A';
-        adj[i][j] = w;
-        adj[j][i] = w;
-    }
+            fscanf(fp, "%d", &adj[i][j]);
     fclose(fp);
 }
 
@@ -95,6 +93,7 @@ int main() {
 
     // 연결 성분 개수 탐색
     int nComp = count_connected_components();
+    printf("(11.1-2) 연결된 성분 개수");
     printf("그래프의 연결 성분 개수: %d\n", nComp);
 
     return 0;
